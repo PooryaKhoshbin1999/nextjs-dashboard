adf1edf69296e9bf47b319b67e31a0caaacd3e69
Poorya Khoshbin <www.pooryakhoshbin@gmail.com>
2024-02-06 11:17:45 +0430

    first commit

---
+++ app/ui/font.ts
@@ -0,0 +1,15 @@
+import { Inter, Roboto } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+import { Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const roboto = Roboto({
+    weight: ['400' , '700'],
+    subsets: ['latin']
+})
